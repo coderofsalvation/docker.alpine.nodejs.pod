d21f185fbf539e6f94395950caa59cb16a3180af
Leon <info@leon.vankammen.eu>
2016-05-02 23:17:36 +0200

    update

---
+++ srv/apps/proxytable.js
@@ -0,0 +1,33 @@
+var http = require('http'),
+    httpProxy = require('http-proxy'),
+    HttpProxyRules = require('./proxy/proxy.js');
+
+// Set up proxy rules instance
+var proxyRules = new HttpProxyRules({
+  rules: {
+    '/pod':              'http://localhost:19999', 
+    '/queue':            'http://localhost:3000', 
+  },
+  default: 'http://localhost:8080' // default target
+});
+
+// Create reverse proxy instance
+var proxy = httpProxy.createProxy();
+
+// Create http server that leverages reverse proxy instance
+// and proxy rules to proxy requests to different targets
+http.createServer(function(req, res) {
+
+  // a match method is exposed on the proxy rules instance
+  // to test a request to see if it matches against one of the specified rules
+  var target = proxyRules.match(req);
+  if (target) {
+    return proxy.web(req, res, {
+      target: target
+    });
+  }
+
+  res.writeHead(500, { 'Content-Type': 'text/plain' });
+  res.end('The request url and path did not match any of the listed rules!');
+}).listen(8080);
+
